a7c05c2c30f2b95c9d3b6350b6f87ba535e72487
krishnakandibanda <krishnakandibanda@gmail.com>
2022-01-23 13:18:20 +0530

    Add some new Packages and class in my Repositorie..

---
+++ src/main/java/com/constructor/NoArgConstructor.java
@@ -0,0 +1,7 @@
+package com.constructor;
+
+public class NoArgConstructor {
+	public static void main(String[] args) {
+		System.out.println("No Argument Constructor...");
+	}
+}
---
+++ src/main/java/com/inheritance/SingleInheritance.java
@@ -0,0 +1,7 @@
+package com.inheritance;
+
+public class SingleInheritance {
+	public static void main(String[] args) {
+		System.out.println("Check single level inheritance");
+	}
+}
